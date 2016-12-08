0aafd44b3e395effd61f27c994916026c79e88f1
user404d <conduffql@gmail.com>
2016-12-07 23:05:00 -0600

    Make async in order to reduce ui lag.

--- LocationPrivacyApp/app/src/main/java/com/djsg38/locationprivacyapp/ServiceUtilities.java
+++ LocationPrivacyApp/app/src/main/java/com/djsg38/locationprivacyapp/ServiceUtilities.java
@@ -21,35 +21,39 @@ public class ServiceUtilities {
             mainPackageName = "com.djsg38.locationprivacyapp";
 
     static void addLocationActivities(Context context, Realm realm) {
-        PackageManager pm = context.getPackageManager();
-        ArrayList<ApplicationInfo> procInfos = (ArrayList<ApplicationInfo>) pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        final PackageManager pm = context.getPackageManager();
+        final ArrayList<ApplicationInfo> procInfos = (ArrayList<ApplicationInfo>) pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        Session session = realm.where(Session.class).findFirst();
-        realm.beginTransaction();
-        for (int i = 0; i < procInfos.size(); i++) {
-            String app = procInfos.get(i).packageName;
+        //make async to avoid ui lag
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Session session = realm.where(Session.class).findFirst();
+                for (int i = 0; i < procInfos.size(); i++) {
+                    String app = procInfos.get(i).packageName;
 
-            Preference preference = realm.where(Preference.class)
-                    .equalTo("packageName", app).findFirst();
+                    Preference preference = realm.where(Preference.class)
+                            .equalTo("packageName", app).findFirst();
 
-            Integer coarse_perms = pm.checkPermission("android.permission.ACCESS_COARSE_LOCATION", app),
-                    fine_perms = pm.checkPermission("android.permission.ACCESS_FINE_LOCATION", app),
-                    sys_perms = procInfos.get(i).flags & ApplicationInfo.FLAG_SYSTEM;
-            if (preference == null && !app.equals(mainPackageName)
-                && (coarse_perms == PackageManager.PERMISSION_GRANTED
-                    || fine_perms == PackageManager.PERMISSION_GRANTED)
-                && sys_perms == 0) {
-                String appName = (String) pm.getApplicationLabel(procInfos.get(i));
-                preference = new Preference();
-                preference.setPackageName(app);
-                preference.setPrivacyScale(5);
-                preference.setName(appName);
-                preference.setService(false);
-                Log.i("preference", preference.toString());
-                session.getPreferences().add(preference);
+                    Integer coarse_perms = pm.checkPermission("android.permission.ACCESS_COARSE_LOCATION", app),
+                            fine_perms = pm.checkPermission("android.permission.ACCESS_FINE_LOCATION", app),
+                            sys_perms = procInfos.get(i).flags & ApplicationInfo.FLAG_SYSTEM;
+                    if (preference == null && !app.equals(mainPackageName)
+                            && (coarse_perms == PackageManager.PERMISSION_GRANTED
+                            || fine_perms == PackageManager.PERMISSION_GRANTED)
+                            && sys_perms == 0) {
+                        String appName = (String) pm.getApplicationLabel(procInfos.get(i));
+                        preference = new Preference();
+                        preference.setPackageName(app);
+                        preference.setPrivacyScale(5);
+                        preference.setName(appName);
+                        preference.setService(false);
+                        Log.i("preference", preference.toString());
+                        session.getPreferences().add(preference);
+                    }
+                }
             }
-        }
-        realm.commitTransaction();
+        });
     }
 
 }
